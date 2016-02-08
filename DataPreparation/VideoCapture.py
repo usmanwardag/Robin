f172313c023711f61ff06a32619957416db3f3ed
Usman Mahmood <usmanwardag@gmail.com>
2016-02-08 21:35:41 +0500

    Camera video capture

--- DataPreparation/VideoCapture.py
+++ DataPreparation/VideoCapture.py
@@ -14,6 +14,7 @@ class VideoCapture:
 
     def captureVideo(self):
         path = os.getcwd()+"\..\Data\ideo1.avi"
+        # If reading from camera, set path to 0.
         self.video = cv2.VideoCapture(path)
 
     def readFrames(self):
