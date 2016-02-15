60e8bdf00fa1cb3fda298e78e1ca5f5232e31287
Usman Mahmood <usmanwardag@gmail.com>
2016-02-15 13:50:13 +0500

    Resolves import error

--- Python/JsonStringFormation/main.py
+++ Python/JsonStringFormation/main.py
@@ -4,14 +4,15 @@ from os import path
 import sys
 from matplotlib import pyplot as plt
 
-sys.path.append(path.abspath('../ObjectMapping'))
+sys.path.insert(0, path.abspath('..'))
+#sys.path.append(path.abspath('../ObjectMapping'))
+print path.abspath('../ObjectMapping')
 
 
 def main():
     from ObjectMapping.ObjectMapping import ObjectMapping
     originalImagePath = os.getcwd()+"\..\Data\personInRoom01.jpg"
     originalImage = plt.imread(originalImagePath)
-
     objectMapping = ObjectMapping(originalImage)
     objectMapping.mapObjects()
     startingPosX, startingPosY, widths, heights = objectMapping.getObjects()
